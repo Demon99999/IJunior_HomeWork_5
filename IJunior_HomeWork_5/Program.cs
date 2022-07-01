4f8584b150422785fcf1798765e66fd4cb4c5b1a
Demon99999 <35568272+Demon99999@users.noreply.github.com>
2022-07-01 16:34:18 +0300

    First commit

---
+++ IJunior_HomeWork_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IJunior_HomeWork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Семенов";
+            string surname ="Дмитрий";
+            string tempLine;
+            Console.WriteLine($"Ваше имя {name}. Ваша фамилия {surname}.");
+            tempLine = name;
+            name = surname;
+            surname = tempLine;
+            Console.WriteLine($"Ваше имя {name}. Ваша фамилия {surname}.");
+        }
+    }
+}
